026ac1b0619c6e9de31c7059feeaee7ac4bd04ac
ransuum <dmitrenkovalera9@gmail.com>
2025-02-03 13:54:28 +0200

    ai

---
+++ src/main/java/org/practice/seeyaa/controller/AIController.java
@@ -0,0 +1,13 @@
+package org.practice.seeyaa.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import lombok.Setter;
+
+public class AIController {
+    @FXML private TextArea responseAi;
+    @Setter private String prompt;
+
+
+
+}
