5150312b1e7c39e149df548e2d632740e26b937f
ransuum <dmitrenkovalera9@gmail.com>
2025-02-12 13:07:36 +0200

    functional interface

--- src/main/java/org/practice/seeyaa/util/moved_letter_conf/MovedLetterConfiguration.java
+++ src/main/java/org/practice/seeyaa/util/moved_letter_conf/MovedLetterConfiguration.java
@@ -2,6 +2,7 @@ package org.practice.seeyaa.util.moved_letter_conf;
 
 import org.practice.seeyaa.enums.TypeOfLetter;
 
+@FunctionalInterface
 public interface MovedLetterConfiguration {
     void setLetterType(String letterId, String email, TypeOfLetter type);
 }
